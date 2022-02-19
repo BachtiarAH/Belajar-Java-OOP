bc8107e66d0ebe5c5967d4f4b1cda3d696472650
BachtiarAH <86826543+BachtiarAH@users.noreply.github.com>
2022-02-19 13:47:54 +0700

    main

--- OOP06_Latihan/src/com/tutorial/Main.java
+++ OOP06_Latihan/src/com/tutorial/Main.java
@@ -38,12 +38,32 @@ class player{
         System.out.println("nama   : "+nama);
         System.out.println("health : "+health);
         System.out.println("level  : "+level);
-        System.out.println("\nSenjata :");
-        System.out.println("nama : "+senjata.nama);
-        System.out.println("attk : "+senjata.attackDamage);
-        System.out.println("\nArmor :");
-        System.out.println("nama : "+Armor.nama);
-        System.out.println("deff : "+Armor.deffensePoint);
+        System.out.println("Senjata : "+senjata.nama+" , "+senjata.attackDamage);
+        System.out.println("Armor : "+Armor.nama+" , "+Armor.deffensePoint);
+        System.out.println();
+    }
+
+    //serang musuh
+    void Attack(player opponen){
+        double power = this.senjata.attackDamage;
+        System.out.println(this.nama +" attacked "+opponen.nama+" with power " +power);
+    }
+
+    //bertahan dari serangan
+    void attack(player opponen){
+        double deffense = opponen.Armor.deffensePoint;
+        double power = this.senjata.attackDamage;
+        double damage;
+
+        if(deffense < power){
+            damage = power - deffense;
+        }else{
+            damage = 0;
+        }
+
+        opponen.health -= damage;
+        System.out.println(this.nama +" attacked "+opponen.nama+" with power " +power);
+        System.out.println(this.nama +" take damage "+damage);
     }
 }
 
@@ -72,15 +92,29 @@ class armor{
 public class Main {
     public static void main(String[] args) {
         //player
-        player Pemain = new player("N Y M P H", 100, 10);
-        
+        player Pemain1 = new player("N Y M P H", 100, 10);
+        player Pemain2 = new player("LEMON",100, 11);
+
         //equipment
         weapon pedangBesi = new weapon("Pedang Besi", 10);
         armor zirahBesi = new armor("Zirah Besi", 11);
+        weapon busur = new weapon("busur", 15);
+        armor zirahRantai = new armor("Zirah Rantai", 5);
 
         //equip Equipment into player
-        Pemain.equipWeaponAndArmor(pedangBesi, zirahBesi);
+        Pemain1.equipWeaponAndArmor(pedangBesi, zirahBesi);
+        Pemain2.equipWeaponAndArmor(busur, zirahRantai);
 
-        Pemain.Display();
+        //tampil stat sebelum pertarungan 
+        Pemain1.Display();
+        Pemain2.Display();
+
+        //pertarungan
+        System.out.println("\nPERTARUNGAN");
+        Pemain1.attack(Pemain2);
+
+        //tampil stat setelah Pertarungan
+        Pemain1.Display();
+        Pemain2.Display();
     }
 }
